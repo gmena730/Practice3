30508146acb8a169c2cfce6100a96abd3d604e9c
nihalcelik <nihalcelik@localhost>
2021-12-01 22:22:44 -0600

    new method...

--- src/three/DemoThree.java
+++ src/three/DemoThree.java
@@ -5,5 +5,10 @@ public class DemoThree {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
 	}
+	
+	public static void addNum(int a, int b) {
+		System.out.println(a+b);
+	}
+
 
 }
